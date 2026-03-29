af3ff6558297c4abd5acfaf1721bba54587dfb2d
Rohith B <rohithb@Rohiths-Mac-mini.local>
2026-03-29 20:36:54 +0530

    spark order

--- transformation.py
+++ transformation.py
@@ -1,4 +1,20 @@
-print('transformation 1')
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.config('spark.ui.port','0').config('spark.shuffle.useOldFetchProtocol','true').enableHiveSupport().master('yarn').appName(' ').getOrCreate()
 
 
-#adding a new feature2 - feature2
+from pyspark.sql.types import *
+
+order_schema_stuc  = StructType([
+    StructField("order_id",LongType()),
+    StructField("order_date",DateType()),
+    StructField("cust_id",IntegerType()),
+    StructField("order_status",StringType())
+])
+
+orders_df = spark.read \
+.format('csv') \
+.schema(order_schema_stuc) \
+.load("/public/trendytech/orders/orders_1gb.csv")
+
+print(orders_df.rdd.getNumPartitions())
